4941db9f9db76f2498de49990eb1e6c3642730ab
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-07-27 16:28:07 -0400

    0.0.9

---
+++ root.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { makeXtalElement } from 'be-importing/makeXtalElement.mjs';
+/** @import {Localizer} from "./node_modules/trans-render/lib/mixins/types" */
+/** @import {XForm} from "./node_modules/trans-render/types" */
+/** @import {Actions, Props} from "./types" */
+/** @import {Actions as A} from './node_modules/trans-render/froop/types' */
+
+const mainTemplate = String.raw `
+`
