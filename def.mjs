0546b91c2f49dd9c72962210ad3b05971e041f8f
bahrus <anderson.bruce.b@gmail.com>
2026-08-04 20:39:08 -0400

    0.0.14

---
+++ def.mjs
@@ -0,0 +1,16 @@
+import {} from './types/font-face-feature/types.d.ts';
+
+const tbd
+
+const features = {
+    fontMgr: {
+        spawn: 'font-face-feature/FontFaceFeature.js',
+        customData: {
+
+        }
+    }
+}
+
+export function render(){
+
+}
